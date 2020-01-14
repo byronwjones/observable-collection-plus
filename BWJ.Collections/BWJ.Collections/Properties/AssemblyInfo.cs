d937853f585dbb38c9a413979b9e2b90a96beebd
Byron Jones <byron@byronwjones.com>
2020-01-14 15:35:29 -0500

    Restructure solution

--- BWJ.Collections/BWJ.Collections/Properties/AssemblyInfo.cs
+++ BWJ.Collections/BWJ.Collections/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("65167390-6724-4b63-8e22-16527f6d7762")]
+[assembly: Guid("a653286b-bc76-4162-994a-169b2aa7fa1e")]
 
 // Version information for an assembly consists of the following four values:
 //
