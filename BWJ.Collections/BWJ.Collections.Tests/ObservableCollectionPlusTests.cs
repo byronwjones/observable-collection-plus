b0275effb12de5b0761685d4aec219787eba9bf3
Byron Jones <byron@byronwjones.com>
2020-01-14 19:50:38 -0500

    Add test project

---
+++ BWJ.Collections/BWJ.Collections.Tests/ObservableCollectionPlusTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BWJ.Collections.Tests
+{
+    [TestClass]
+    public class ObservableCollectionPlusTests
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BWJ.Collections/BWJ.Collections.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BWJ.Collections.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BWJ.Collections.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d94129f0-a7ca-4820-b1e2-0f8d84eb860b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
